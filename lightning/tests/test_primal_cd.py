Fix bug in multiclass logistic derivative.

lightning/tests/test_primal_cd.py
@@ -400,21 +400,22 @@ def test_l1l2_multiclass_log_loss():
         clf = CDClassifier(penalty="l1/l2", loss="log", multiclass=True,
                            max_iter=5, C=1.0, random_state=0)
         clf.fit(data, mult_target)
-        assert_almost_equal(clf.score(data, mult_target), 0.82666, 3)
+        assert_almost_equal(clf.score(data, mult_target), 0.8733, 3)
         df = clf.decision_function(data)
         sel = np.array([df[i, int(mult_target[i])] for i in xrange(df.shape[0])])
         df -= sel[:, np.newaxis]
         df = np.exp(df)
         assert_array_almost_equal(clf.errors_, df.T)
-        assert_equal(np.sum(clf.coef_ != 0), 300)
+        assert_equal(np.sum(clf.coef_ != 0), 297)
 
         clf = CDClassifier(penalty="l1/l2", loss="log", multiclass=True,
                            max_iter=5, C=0.3, random_state=0)
         clf.fit(data, mult_target)
-        assert_almost_equal(clf.score(data, mult_target), 0.8033, 3)
+        assert_almost_equal(clf.score(data, mult_target), 0.8366, 3)
         nz = np.sum(clf.coef_ != 0)
-        assert_equal(nz, 246)
+        assert_equal(nz, 243)
         assert_true(nz % 3 == 0) # should be a multiple of n_classes
+
 
 def test_l1l2_multiclass_squared_hinge_loss():
     for data in (mult_dense, mult_csc):