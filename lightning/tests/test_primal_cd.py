Fix bug in derivative.

lightning/tests/test_primal_cd.py
@@ -423,7 +423,7 @@ def test_l1l2_multiclass_squared_hinge_loss():
                            multiclass=True,
                            max_iter=20, C=1.0, random_state=0)
         clf.fit(data, mult_target)
-        assert_almost_equal(clf.score(data, mult_target), 0.833, 3)
+        assert_almost_equal(clf.score(data, mult_target), 0.9, 3)
         df = clf.decision_function(data)
         n_samples, n_vectors = df.shape
         diff = np.zeros_like(clf.errors_)
@@ -437,9 +437,9 @@ def test_l1l2_multiclass_squared_hinge_loss():
                            multiclass=True,
                            max_iter=20, C=0.05, random_state=0)
         clf.fit(data, mult_target)
-        assert_almost_equal(clf.score(data, mult_target), 0.81, 3)
+        assert_almost_equal(clf.score(data, mult_target), 0.83, 3)
         nz = np.sum(clf.coef_ != 0)
-        assert_equal(nz, 249)
+        assert_equal(nz, 219)
         assert_true(nz % 3 == 0) # should be a multiple of n_classes
 
 
@@ -451,14 +451,6 @@ def test_l1l2_multiclass_squared_hinge_loss_kernel():
         clf.fit(data, mult_target)
         assert_equal(clf.score(data, mult_target), 1.0)
         assert_equal(clf.n_nonzero(), 300)
-
-        clf = CDClassifier(penalty="l1/l2", loss="squared_hinge", multiclass=True,
-                           kernel="rbf", gamma=0.1,
-                           max_iter=20, C=0.3, random_state=0)
-        clf.fit(data, mult_target)
-        assert_almost_equal(clf.score(data, mult_target), 0.67)
-        assert_equal(clf.n_nonzero(), 201)
-        assert_true(clf.n_nonzero() % 3 == 0)
 
 
 def test_l1l2_multi_task_squared_hinge_loss():