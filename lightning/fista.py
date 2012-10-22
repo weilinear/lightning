faa7097e7d3a8b87bb1d0766ac50984aa5f35c1c
Mathieu Blondel <mathieu@mblondel.org>
2012-10-22 16:16:08 +0900

    Fixed bug.

--- lightning/fista.py
+++ lightning/fista.py
@@ -91,7 +91,7 @@ class FistaClassifier(BaseClassifier, ClassifierMixin):
 
         if self.max_steps == 0:
             # No line search, need to use constant step size.
-            L = loss.max_gradient(ds, n_vectors)
+            L = self.C * loss.max_gradient(ds, n_vectors)
         else:
             # Do not bother to compute the Lipschitz constant (expensive).
             L = 1.0
